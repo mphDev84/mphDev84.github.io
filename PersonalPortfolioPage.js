9920eb752f0ef8661c25c90f84cc301dda99b9f8
Matt <91234966+mphDev84@users.noreply.github.com>
2021-12-27 21:35:34 -0500

    Personal Portfolio Page
    This is my work.

---
+++ PersonalPortfolioPage.js
@@ -0,0 +1,51 @@
+function light(){ 
+    document.getElementById("para1").style.color="black";
+    document.documentElement.style
+      .setProperty('--main-background-color', 'linear-gradient(to right, #F7F7F7 0%, #F4F4F4 50%, #E5E5E5 100%)');
+  
+  document.documentElement.style
+      .setProperty('--main-text-color', 'black');  
+    var x =document.getElementsByClassName("button");
+    x[0].style.color='black';
+    x[1].style.color='black';
+    x[2].style.color='black';  
+  }
+  function dark(){
+     document.documentElement.style
+      .setProperty('--main-background-color', 'linear-gradient(to right, #141414 0%, #323232 50%, #636363 100%)');
+  
+  document.documentElement.style
+      .setProperty('--main-text-color', 'white');
+     var x =document.getElementsByClassName("button");
+    x[0].style.color='#CCFFFF';
+    x[1].style.color='#CCFFFF';
+    x[2].style.color='#CCFFFF';  
+     document.getElementById("para1").style.color="white";
+  }
+   var x, i;
+    x = document.querySelectorAll(".block");
+    for (i = 0; i < x.length; i++) {
+      x[i].addEventListener("mouseover", changeFont);}
+   var x, j;
+    x = document.querySelectorAll(".block");
+    for (j = 0; j < x.length; j++) {
+      x[j].addEventListener("mouseout", changeFontBack);}
+  
+  function changeFont(){
+    var ranNum = (Math.floor(Math.random()*5)+1).toFixed(0);
+    if(ranNum==1){
+  document.querySelector("#para1").style.color = "green"; }
+  if(ranNum==2){
+  document.querySelector("#para1").style.color = "yellow";}
+  if(ranNum==3){
+  document.querySelector("#para1").style.color = "lightblue";} 
+  if(ranNum==4){
+  document.querySelector("#para1").style.color = "grey"; }
+  if(ranNum==5){
+  document.querySelector("#para1").style.color = "pink"; } 
+    }
+  
+    function changeFontBack(){
+     document.querySelector("#para1").style.fontFamily ="verdana"; document.querySelector("#para1").style.color = "white";
+    }
+  
