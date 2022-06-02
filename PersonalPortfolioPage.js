19933b7b5c74e93e2b52e27a421cf06cdb2e86ca
Matt <91234966+mphDev84@users.noreply.github.com>
2022-06-02 14:25:18 -0400

    new project added
    new project - Nextjs Dinosaur website - added to portfolio page.

--- PersonalPortfolioPage.js
+++ PersonalPortfolioPage.js
@@ -1,88 +1,94 @@
-function light(){ //called from onClick event
-  //change background color
-    document.documentElement.style
-      .setProperty('--main-background-color', 'linear-gradient(to right, #D9DBDB 0%, #D0D6E5 50%, #EAE8E9 100%)');
-  
-    //change 'button' color 
-  document.documentElement.style
-      .setProperty('--main-text-color', 'black');  
-    let x =document.getElementsByClassName("button");
-    x[0].style.color='black';
-    x[1].style.color='black';
-    x[2].style.color='black';  
-    x[3].style.color='black';  
-  }
+function light() {
+	//called from onClick event
+	//change background color
+	document.documentElement.style.setProperty(
+		"--main-background-color",
+		"linear-gradient(to right, #D9DBDB 0%, #D0D6E5 50%, #EAE8E9 100%)"
+	);
 
-  function dark(){//called from onClick event 
-    //change background color back to that at page load
-     document.documentElement.style
-      .setProperty('--main-background-color', 'linear-gradient(to right, #141414 0%, #323232 50%, #636363 100%)');
-  
-      //change text colors 
-  document.documentElement.style.setProperty('--main-text-color', 'white');
-     let x =document.getElementsByClassName("button");
-    x[0].style.color='#CCFFFF';
-    x[1].style.color='#CCFFFF';
-    x[2].style.color='#CCFFFF';  
-    x[3].style.color='#CCFFFF';  
-     document.getElementById("para1").style.color="white";
-     document.getElementById("header1").style.color="white";
-  };
+	//change 'button' color
+	document.documentElement.style.setProperty("--main-text-color", "black");
+	let x = document.getElementsByClassName("button");
+	x[0].style.color = "black";
+	x[1].style.color = "black";
+	x[2].style.color = "black";
+	x[3].style.color = "black";
+}
 
-  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+function dark() {
+	//called from onClick event
+	//change background color back to that at page load
+	document.documentElement.style.setProperty(
+		"--main-background-color",
+		"linear-gradient(to right, #141414 0%, #323232 50%, #636363 100%)"
+	);
+
+	//change text colors
+	document.documentElement.style.setProperty("--main-text-color", "white");
+	let x = document.getElementsByClassName("button");
+	x[0].style.color = "#CCFFFF";
+	x[1].style.color = "#CCFFFF";
+	x[2].style.color = "#CCFFFF";
+	x[3].style.color = "#CCFFFF";
+	document.getElementById("para1").style.color = "white";
+	document.getElementById("header1").style.color = "white";
+}
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
 
 function scrollToTop() {
-  // Scroll to top logic
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-};
+	// Scroll to top logic
+	rootElement.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+}
 
 function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
-
-var sliderImages = document.querySelectorAll('.slide-in');
-
-function checkSlide(){  
-    
-  sliderImages.forEach(slideImage=>{
-      //half-way through image
-      /*NOTE - I have hard-coded the pixel values below, I can't find a way of accessing the height
-      of the 'slideImage' div element on the html page. These hacks work for now.*/  
-  const slideImageHeight = 705;
-  const slideInAt = ((window.scrollY + window.innerHeight) - slideImageHeight) / 2;
-      //bottom of image
-   const imageBottom = 1600;
-   const newHeight = 305;
-   const isHalfShown = slideInAt > newHeight;
-   const isNotScrolledPast = window.scrollY <imageBottom;
-   if(isHalfShown&&isNotScrolledPast){
-    slideImage.classList.add('active');
-}else{
-   slideImage.classList.remove('active');
+	var timeout;
+	return function () {
+		var context = this,
+			args = arguments;
+		var later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
 }
-  });
-};
+
+var sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide() {
+	sliderImages.forEach((slideImage) => {
+		//half-way through image
+		/*NOTE - I have hard-coded the pixel values below, I can't find a way of accessing the height
+      of the 'slideImage' div element on the html page. These hacks work for now.*/
+		const slideImageHeight = 705;
+		const slideInAt =
+			(window.scrollY + window.innerHeight - slideImageHeight) / 2;
+		//bottom of image
+		const imageBottom = 1600;
+		const newHeight = 305;
+		const isHalfShown = slideInAt > newHeight;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		if (isHalfShown && isNotScrolledPast) {
+			slideImage.classList.add("active");
+		} else {
+			slideImage.classList.remove("active");
+		}
+	});
+}
 
 window.addEventListener("scroll", debounce(checkSlide));
 
-  //the code below is related to the commented-out 'divs' in index.html (lines 49-69), with the class "block".
+//the code below is related to the commented-out 'divs' in index.html (lines 49-69), with the class "block".
 
-  /*
+/*
    var x, i;
     x = document.querySelectorAll(".block");
     for (i = 0; i < x.length; i++) {
@@ -93,8 +99,8 @@ window.addEventListener("scroll", debounce(checkSlide));
       x[j].addEventListener("mouseout", changeFontBack);}
 
       */
-  
-    /*
+
+/*
   function changeFont(){
     var ranNum = (Math.floor(Math.random()*10)+1).toFixed(0);
     if(ranNum==1){
@@ -122,8 +128,7 @@ document.querySelector("#header1").style.color = "red"; }
   }
 */
 
-  /* function changeFontBack(){
+/* function changeFontBack(){
      document.querySelector("#para1").style.fontFamily ="verdana"; document.querySelector("#para1").style.color = ("--main-text-color");
      document.querySelector("#header1").style.color = ("--main-text-color");
     } */
-  
